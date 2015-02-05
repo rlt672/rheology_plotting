662202b988fd4c86ce574a513a93ebd11df1e76a
rlt672 <rtruby@Ryan-Trubys-MacBook-Pro.local>
2015-02-05 00:26:14 -0500

    Draft of PlotRheology.py
    Began draft based on J Muth’s plotting code

---
+++ PlotRheology.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+
+Filename: PlotRheology.py
+Author: Ryan L. Truby
+Affiliation: Lewis Research Group, Harvard University
+Data: 2015.02.01
+
+Description:
+    This library contains functions for plotting rheological data obtained on 
+    the DHR-3 rheometer in Prof. Jennifer Lewis' lab at Havard University. The
+    code was initially adapted from Joseph Muth's Python script, titled,
+    "Rheology Reader.py"
+    
+    This code takes input files of ".xls" format.
+    
+    The xls file read should have the following format:
+        Row 1: Title of sheet (e.g., "Amplitude sweep - 1")
+        Row 2: Storage modulus | Loss modulus | Tan(delta) | Complex viscosity | 
+                   Oscillation strain | Angular frequency | Time | Oscillation 
+                   strain | Oscillation stress
+        Row 3: Pa | Pa |  | Pa.s | 1/s | rad/s | s | % | Pa
+        Row 4 to end: (All of the data) 
+    
+"""
+
+import numpy as np
+import os
+import matplotlib.pylab as plt
+import pandas as pd
+
+def import_rheo_data(filename, pagename, start):
+    rheo_data = pd.readcsv(filename, pagename)
+
+    
