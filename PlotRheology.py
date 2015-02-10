ca736138e22387632f21a48d5c269bc12d854f58
rlt672 <rtruby@Ryans-MacBook-Pro.local>
2015-02-09 22:22:47 -0500

    Plotting achieved

--- PlotRheology.py
+++ PlotRheology.py
@@ -26,19 +26,10 @@ Description:
 
 import numpy as np
 import os
+import matplotlib
 import matplotlib.pylab as plt
+import matplotlib.font_manager as font_manager
 import pandas as pd
-
-#def import_filenames():
-#    file_name = ''
-#    file_name_list = list()
-#    print "Enter names of Excel files (w/ extension) containing data (STOP to quit): "
-#    while ((file_name != 'STOP') & (file_name != 'stop') & (file_name != 'Stop')):
-#        file_name = raw_input()
-#        if ((file_name != 'STOP') & (file_name != 'stop') & (file_name != 'Stop')):
-#            file_name_list.append(file_name)
-#    print '\nImporting the following files:\n%r' % file_name_list
-#    return file_name_list
 
 # Directory variables
 folder_name = 'rheology_data'
@@ -53,7 +44,24 @@ skip_rows = [0, 2]
 # Study variables
 materials = ['Fugitive Ink', 'Catalytic Ink', 'PDMS Matrix', 'EcoFlex Matrix']
 variables = ['Storage modulus', 'Loss modulus', 'Oscillatin strain rate', 'Oscillation stress', 'Oscillation stress']
-ignored_var = ['Tan(delta)', 'Complex viscosity', 'Angular frequency', 'Time'] 
+ignored_var = ['Tan(delta)', 'Complex viscosity', 'Angular frequency', 'Time']
+
+# Plot variables
+size_of_font = 14
+axes_font_factor = 2
+legend_font_factor = -2
+font = 'Arial'
+
+# Uncomment the following code to see what fonts are available for plots:
+'''
+for font in font_manager.findSystemFonts():
+    print font
+'''
+
+# Choose the fontpath you want for the plot's font
+fontpath = '/Library/Fonts/Microsoft/Arial.ttf'
+prop = font_manager.FontProperties(fname = fontpath)
+matplotlib.rcParams['font.family'] = prop.get_name()
 
 # Create file_names, a list that holds all of the file names with rheology data
 current_dir = os.getcwd()
@@ -70,10 +78,24 @@ PDMS_Matrix = pd.read_excel(file_names[2], sheet_name, header = label_row, skipr
 EcoFlex_Matrix = pd.read_excel(file_names[3], sheet_name, header = label_row, skiprows = skip_rows)
 
 # Plot the Fugitive Ink Rheology: storage modulus vs. stress
-x = Fugitive_Ink['Oscillation stress']
-y = Fugitive_Ink['Storage modulus']
-plt.plot(x, y)
- 
+x1 = Fugitive_Ink['Oscillation stress']
+y1 = Fugitive_Ink['Storage modulus']
+x2 = Catalytic_Ink['Oscillation stress']
+y2 = Catalytic_Ink['Storage modulus']
+x3 = PDMS_Matrix['Oscillation stress']
+y3 = PDMS_Matrix['Storage modulus']
+x4 = EcoFlex_Matrix['Oscillation stress']
+y4 = EcoFlex_Matrix['Storage modulus']
+
+Plot1 = plt.figure(figsize=[5.55, 4.75], tight_layout=True)
+plt.loglog(x1, y1, 'ko-')
+plt.loglog(x2, y2, 'r^-')
+plt.loglog(x3, y3, 'bs-')
+plt.loglog(x4, y4)
+plt.xlabel('Shear Stress [Pa]', fontsize = size_of_font + axes_font_factor, family = font)
+plt.ylabel('G\' [Pa]', fontsize = size_of_font + axes_font_factor, family = font)
+plt.legend(['Fugitive Ink', 'Catalytic Ink', 'PDMS Matrix', 'EcoFlex Matrix'], fontsize = size_of_font + legend_font_factor)
+plt.show()
 
 
 
